f6666c612d66871322b18dd1354ea0da02608d0e
Alessandro Leonardi <alessandroleonardi59@gmail.com>
2020-07-20 16:43:28 +0200

    Initial Commit

---
+++ P3_RPGMACOS/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  P3_RPGMACOS
+//
+//  Created by Alessandro Leonardi on 20/07/2020.
+//  Copyright © 2020 Alessandro Leonardi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
